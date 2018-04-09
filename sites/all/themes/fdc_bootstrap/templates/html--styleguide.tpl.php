fd5b211bba0d3af56517dc80f048768cb1474f6f
Ben Lamptey <ben@codepulse.co.uk>
2018-04-09 17:31:10 +0100

    updated menu blocks for homepage

--- sites/all/themes/fdc_bootstrap/templates/html--styleguide.tpl.php
+++ sites/all/themes/fdc_bootstrap/templates/html--styleguide.tpl.php
@@ -1,5 +1,7 @@
-<head>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-</head>
-<?php print $page; ?>
+<html class="new-pse padding-top">
+  <head>
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+  </head>
+  <?php print $page; ?>
+</html>
--- sites/all/themes/fdc_bootstrap/templates/page--styleguide.tpl.php
+++ sites/all/themes/fdc_bootstrap/templates/page--styleguide.tpl.php
@@ -1,9 +1,18 @@
 <body>
   <div class="pse pse-wrapper">
     <div class="container">
-      <navigation>
-        <?php print render($page['content']); ?>
-      </navigation>
+      <div class="pse-nav-bar">
+        <div class="pse-main-nav__logo">
+<!--          <a href=""><img src="--><?php //print '/' . drupal_get_path('theme', 'fdc_bootstrap') . '/logo.svg'?><!--" class="pse__logo"/></a>-->
+        </div>
+        <nav class="pse-main-nav__main-menu">
+          <?php print render($page['main_menu_region']); ?>
+        </nav>
+        <div class="pse-main-nav__personalised-menu">
+          <?php print l(t('Sign in'), 'user/login'); ?>
+        </div>
+      </div>
+<!--      --><?php //print render($page['content']); ?>
     </div>
   </div>
 </body>
